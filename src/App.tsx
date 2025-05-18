44a402fb2ecf812549c5e4e4dabbc1bd945bae20
Developer-Utkarsh <utkarshweb2023@gmail.com>
2025-05-18 08:49:05 +0530

    Intial Structure

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <Button type="submit">Hello World</Button>
+    </>
+  );
+}
+
+export default App;
